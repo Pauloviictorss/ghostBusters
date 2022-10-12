d3e45a5714a3c14bb908ddcb26d3c80e8ed04f95
“Pauloviictorss” <paulovictorlvm@gmail.com>
2022-10-12 13:42:44 -0300

    Criando tabela de Aluguel de Equipamentos. Seeds devem ser realizados individualmente

--- database/migrations/1665590251302_aluguel_equipamentos.ts
+++ database/migrations/1665590251302_aluguel_equipamentos.ts
@@ -1,18 +1,16 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'funcionarios'
+  protected tableName = 'aluguel_equipamentos'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.string('cpf').notNullable()
-      table.string('nome').notNullable()
-      table.string('telefone')
-      table.string('sexo', 1).notNullable()
-      table.string('endereco')
-      table.integer('cargo_id').unsigned().references('id').inTable('cargos').notNullable()
+      table.integer('cliente_id').unsigned().references('id').inTable('clientes')
+      table.date('data_retirada').notNullable()
+      table.date('data_entrega').notNullable()
+      table.integer('dias_utilizacao').notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
