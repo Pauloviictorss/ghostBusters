3a0b9f3ac26535bb0d7a0eee38f740b3c2493570
zuckzada <marcusviniciusaprendiz@gmail.com>
2022-10-19 14:14:52 -0300

    Routes modificadas e ProdutorasController criada.

--- app/Controllers/Http/ProdutorasController.ts
+++ app/Controllers/Http/ProdutorasController.ts
@@ -1,3 +1,32 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Produtora from "App/Models/Produtora"
 
-export default class ProdutorasController {}
+
+export default class CategoriasController {
+    index(){
+        return Produtora.all()
+    }
+    store({request}){
+        const dados = request.only(['nome'])
+
+        return Produtora.create(dados)
+    }
+    show({request}){
+        const id = request.param('id')
+        return Produtora.find(id)
+    }
+    async destroy({request}){
+        const id = request.param('id')
+        const produtora = await Produtora.findOrFail(id)
+        return produtora.delete()
+    }
+    async update({request}){
+        const id = request.param('id')
+        const produtora = await Produtora.findOrFail(id)
+
+        const dados = request.only(['produtora'])
+        
+        produtora.merge(dados).save()
+
+        return dados
+    }
+}
